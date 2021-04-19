80ae95ad6dca084733925fe0657a67d81036fe21
kuldeepsahu <kuldeepsahu525749@gmail.com>
2021-04-19 08:27:50 +0530

    first commit

---
+++ nodeServer/index.js
@@ -0,0 +1,18 @@
+
+
+// Node server which where node socket to connections
+const io = require('socket.io')(8000)
+
+const users = {};
+io.on('connection', socket=>{
+    socket.on('new-user-joined', name =>{
+    console.log("user",name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    })
+    socket.on('send', message=>{
+        socket.broadcast.emit('receive',{message:message, name: user[socket.id]});
+    });
+})
+
+
